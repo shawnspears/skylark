eebc4c36fd7b315fe447fbf1b425715b7bfb6800
natasha-t <nthapli@gmail.com>
2016-02-19 19:29:32 -0800

    Change root directory for heroku

--- practice/trial/app/assets/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-  $('.directUpload').find("input:file").each(function(i, elem) {
-    var fileInput = $(elem);
-    console.log(fileInput);
-  });
-
-
-});
---
+++ practice/vendor/app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+// = require jquery
+//= require jquery_ujs
+//= require jquery.ui.widget
+//= require z.jquery.fileupload
+//= require_tree .
